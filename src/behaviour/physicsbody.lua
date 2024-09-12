9c151103fa305d1ba7c9d7db6fa0e341105772ed
CimimUxMaio <juanbiabdon@gmail.com>
2024-09-12 12:52:35 -0300

    Simplify PhysicsBody behaviour

--- src/behaviour/physicsbody.lua
+++ src/behaviour/physicsbody.lua
@@ -1,88 +1,33 @@
 local Behaviour = require("behaves2.behaviour")
 
 --- @class PhysicsBody : Behaviour
---- @field protected shape love.Shape
---- @field protected fixture love.Fixture
+--- @field protected body love.Body
 local PhysicsBody = setmetatable({}, Behaviour)
 PhysicsBody.__index = PhysicsBody
 PhysicsBody.className = "PhysicsBody"
 
---- @param fixture love.Fixture
+--- @param body love.Body
 --- @return PhysicsBody
-function PhysicsBody:new(fixture)
-	--- @type PhysicsBody
+function PhysicsBody:new(body)
 	local instance = Behaviour.new(self)
-	local userData = fixture:getUserData() or {}
-	userData._physicsBody = instance
-	fixture:setUserData(userData)
-	instance.fixture = fixture
+	instance.body = body
 	return instance
 end
 
 function PhysicsBody:onDestroy()
-	self.fixture:getBody():destroy()
-end
-
---- @return love.Fixture
-function PhysicsBody:getFixture()
-	return self.fixture
+	self.body:destroy()
 end
 
 function PhysicsBody:getBody()
-	return self.fixture:getBody()
-end
-
-function PhysicsBody:getShape()
-	return self.fixture:getShape()
+	return self.body
 end
 
 function PhysicsBody:getPosition()
 	return self:getBody():getPosition()
 end
 
---- @param fixture love.Fixture
---- @param other love.Fixture
---- @param contact love.Contact
-function PhysicsBody:collisionEnter(fixture, other, contact)
-	self.entity:raiseEvent("collisionEnter", fixture, other, contact)
-end
-
---- @param fixture love.Fixture
---- @param other love.Fixture
---- @param contact love.Contact
-function PhysicsBody:collisionExit(fixture, other, contact)
-	self.entity:raiseEvent("collisionExit", fixture, other, contact)
-end
-
---- @param world love.World
-function PhysicsBody.setCallbacks(world)
-	local onCollisionEnter = function(fixA, fixB, contact)
-		local dataA = fixA:getUserData()
-		local dataB = fixB:getUserData()
-
-		if dataA and dataA._physicsBody then
-			dataA._physicsBody:collisionEnter(fixA, fixB, contact)
-		end
-
-		if dataB and dataB._physicsBody then
-			dataB._physicsBody:collisionExit(fixB, fixA, contact)
-		end
-	end
-
-	local onCollisionExit = function(fixA, fixB, contact)
-		local dataA = fixA:getUserData()
-		local dataB = fixB:getUserData()
-
-		if dataA and dataA._physicsBody then
-			dataA._physicsBody:collisionExit(fixA, fixB, contact)
-		end
-
-		if dataB and dataB._physicsBody then
-			dataA._physicsBody:collisionExit(fixB, fixA, contact)
-		end
-	end
-
-	world:setCallbacks(onCollisionEnter, onCollisionExit)
+function PhysicsBody:setPosition(x, y)
+	self.body:setPosition(x, y)
 end
 
 return PhysicsBody
